a7731913323a2be272cc877eeed3e69d6cab5b6a
TruongTanNghia <tannghiaxp@gmail.com>
2023-11-25 03:13:54 +0700

    #3 [Setup] - Adding Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-mont)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
+    },
   },
   plugins: [],
-}
-
+};
