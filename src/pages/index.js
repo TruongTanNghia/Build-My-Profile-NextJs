4e463fbe3f9ff43a5ed0955f900bb72e1b0d8cf2
TruongTanNghia <tannghiaxp@gmail.com>
2023-11-30 18:59:43 +0700

    feat: #7 [Animated] - Creating Animated Text Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 export default function Home() {
   return (
     <>
@@ -19,14 +22,35 @@ export default function Home() {
                 className="w-full h-auto"
               />
             </div>
-            <div className="w-1/2">
-              <h1>Turning Vision Into Reality With Code And Design. </h1>
-              <p>
+            <div className="w-1/2 flex flex-col items-center self-center ">
+              <h1></h1>
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="text-6xl !text-left"
+              />
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/dummy.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-black text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                >
+                  Resume
+                  <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link
+                  href="mailto:tannghia1005xp@gmail.com"
+                  target={"_blank"}
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
