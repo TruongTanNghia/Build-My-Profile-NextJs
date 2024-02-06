ec8b35d88624868285c47ff9b828600556947acd
TruongTanNghia <tannghiaxp@gmail.com>
2024-02-06 16:12:17 +0700

    feat: #28 [PAGE] - Make Page Certificate

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -92,7 +92,7 @@ const NavBar = () => {
             <CustomLink href="/" title="Home" className="mr-4" />
             <CustomLink href="/about" title="About" className="mx-4" />
             <CustomLink href="/project" title="Project" className="mx-4" />
-            <CustomLink href="/articles" title="Articles" className="ml-4" />
+            <CustomLink href="/articles" title="Articles" className="mx-4" />
             <CustomLink
               href="/certificate"
               title="Certificate"
